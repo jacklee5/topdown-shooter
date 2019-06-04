d705fb8a92d52cc3f352c749a99228950bc01469
jacklee5 <jack200336@gmail.com>
2019-06-04 13:48:43 -0400

    beep

--- static/script.js
+++ static/script.js
@@ -1,5 +1,19 @@
 const socket = io();
 
+//page switching stuff
+const PAGES = {
+    HOME: 0,
+    GAME: 1
+}
+let currentPage = PAGES.HOME;
+const changePage = (id) => {
+    const pages = document.getElementsByClassName("page");
+    for(let i = 0; i < pages.length; i++){
+        pages[i].style.display = "none";
+    }
+    pages[id].style.display = "block";
+}
+
 //join game button
 document.getElementById("join-form").addEventListener("submit", (e) => {
     e.preventDefault();
@@ -9,5 +23,9 @@ document.getElementById("join-form").addEventListener("submit", (e) => {
 
 //listen for joining game
 socket.on("game found", (roomId) => {
-    alert("joined room " + roomId);
+    changePage(PAGES.GAME);
 });
+
+//set up the game
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
